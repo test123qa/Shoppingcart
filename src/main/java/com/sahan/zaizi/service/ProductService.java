3d2fc99fb93a1d6f21401f8db2e23bfd2fef1c84
nisum <nisum@192.168.100.4>
2017-10-20 16:27:11 +0530

    code changes for shopping cart

--- src/main/java/com/sahan/zaizi/service/ProductService.java
+++ src/main/java/com/sahan/zaizi/service/ProductService.java
@@ -30,16 +30,16 @@ public class ProductService {
 	private static List<Product> products = new ArrayList<>();
 
 	static {
-		products.add(new Product(35.75d, 1000, "Pears baby soap for Kids", "Soap", 'a'));
-		products.add(new Product(45.50d, 500, "Signal Tooth Brushes Size in (L, M, S)", "Tooth Brushe", 'a'));
-		products.add(new Product(1500.0d, 100, "Casual Shirt imported from France", "Shirt", 'a'));
-		products.add(new Product(1000.0d, 400, "Leather bag imported from USA ", "Office Bag", 'a'));
-		products.add(new Product(450.0d, 800, "Hot Water Bottles", "Bottle", 'a'));
-		products.add(new Product(2500.0d, 800, "Imported wrist watches from swiss", "Wrist Watch", 'a'));
-		products.add(new Product(45000.0d, 800, "3G/4G capability", "Mobile Phone", 'a'));
-		products.add(new Product(300.0d, 800, "Head and Shoulders Shampoo", "Shampoo", 'a'));
-		products.add(new Product(550.0d, 800, "Imported Leather Wallets from AUS", "Leather Wallets", 'a'));
-		products.add(new Product(85000.0d, 800, "Imported Canon camera from USA", "Camera", 'a'));
+		products.add(new Product(35.75d, 1000, "Pears baby soap for Kids", "Soap", "MEN","MEN_ACTIVE_WARE"));
+		products.add(new Product(45.50d, 500, "Signal Tooth Brushes Size in (L, M, S)", "Tooth Brushe", "WOMEN","WOMEN_ACTIVE_WARE"));
+		products.add(new Product(1500.0d, 100, "Casual Shirt imported from France", "Shirt", "WOMEN","WOMEN_ACTIVE_WARE"));
+		products.add(new Product(1000.0d, 400, "Leather bag imported from USA ", "Office Bag", "KIDS","KIDS_ACTIVE_WARE"));
+		products.add(new Product(450.0d, 800, "Hot Water Bottles", "Bottle", "MEN","MEN_ACTIVE_WARE"));
+		products.add(new Product(2500.0d, 800, "Imported wrist watches from swiss", "Wrist Watch", "MEN","MEN_ACTIVE_WARE"));
+		products.add(new Product(45000.0d, 800, "3G/4G capability", "Mobile Phone", "MEN","MEN_ACTIVE_WARE"));
+		products.add(new Product(300.0d, 800, "Head and Shoulders Shampoo", "Shampoo", "MEN","MEN_ACTIVE_WARE"));
+		products.add(new Product(550.0d, 800, "Imported Leather Wallets from AUS", "Leather Wallets", "MEN","MEN_ACTIVE_WARE"));
+		products.add(new Product(85000.0d, 800, "Imported Canon camera from USA", "Camera", "MEN","MEN_ACTIVE_WARE"));
 	}
 
 	public void saveInitialBatch() {
@@ -101,6 +101,15 @@ public class ProductService {
 		try {
 			return productRepository.findByName(name);
 		} catch (Exception ex) {
+			throw new ShoppingCartException("some thing went wrong while fetching data");
+		}
+	}
+	
+	
+	public List<Product> getProductByCategory(String category, String subCategory) throws ShoppingCartException{
+		try {
+			return productRepository.findByCategory(category, subCategory);
+		}catch(Exception e) {
 			throw new ShoppingCartException("some thing went wrong while fetching data");
 		}
 	}
