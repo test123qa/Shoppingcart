266f78c08d3b35422ed96c19d4c1d52e84e6b28c
nisum <nisum@192.168.100.4>
2017-10-20 16:26:39 +0530

    code changes for shopping cart

--- src/main/java/com/sahan/zaizi/domain/Product.java
+++ src/main/java/com/sahan/zaizi/domain/Product.java
@@ -12,86 +12,99 @@ import java.util.Set;
 @Table(name = "product")
 public class Product implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
 
-    @Column(name = "name")
-    private String name;
-   //private char gender;
-   
-    @Column(name = "description")
-    private String description;
+	@Column(name = "name")
+	private String name;
+	// private char gender;
 
-    @Column(name = "quantity")
-    private Integer quantity;
+	@Column(name = "description")
+	private String description;
 
-    @Column(name = "unit_price")
-    private Double unitPrice;
+	@Column(name = "quantity")
+	private Integer quantity;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "product")
-    private Set<ShoppingCart> shoppingCarts = new HashSet<>();
-   @Column(name="category",length=1,nullable=false)
-    private char category;
+	@Column(name = "unit_price")
+	private Double unitPrice;
 
-    public char getCategory() {
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "product")
+	private Set<ShoppingCart> shoppingCarts = new HashSet<>();
+	@Column(name = "category", length = 45, nullable = false)
+	private String category;
+
+	@Column(name = "subCategory", nullable = false, length = 45)
+	private String subCategory;
+
+	public String getSubCategory() {
+		return subCategory;
+	}
+
+	public void setSubCategory(String subCategory) {
+		this.subCategory = subCategory;
+	}
+
+	public String getCategory() {
 		return category;
 	}
 
-	public void setCategory(char category) {
+	public void setCategory(String category) {
 		this.category = category;
 	}
 
-	public Product(){
+	public Product() {
 
-    }
+	}
 
-    public Product(Double unitPrice, Integer quantity, String description, String name,char catatory) {
-        this.unitPrice = unitPrice;
-        this.quantity = quantity;
-        this.description = description;
-        this.name = name;
-        this.category=catatory;
-    }
+	public Product(Double unitPrice, Integer quantity, String description, String name, String catatory,
+			String subCategory) {
+		this.unitPrice = unitPrice;
+		this.quantity = quantity;
+		this.description = description;
+		this.name = name;
+		this.category = catatory;
+		this.subCategory = subCategory;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public Double getUnitPrice() {
-        return unitPrice;
-    }
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
 
-    public void setUnitPrice(Double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
 
-    public Integer getQuantity() {
-        return quantity;
-    }
+	public Integer getQuantity() {
+		return quantity;
+	}
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
 }
