88c24d3778b0e4de444e7da12b65727d1933387e
nisum <nisum@192.168.2.10>
2017-10-25 16:34:03 +0530

    ProductId and userId missing

--- src/main/resources/static/app/scripts/controllers/userBag.js
+++ src/main/resources/static/app/scripts/controllers/userBag.js
@@ -11,14 +11,16 @@ proApp.controller('userBagCtrl', function($scope, $http, $window) {
 	var iniProData = undefined;
 	var module = undefined;
 	var subModule = undefined;
+	var productId = undefined;
+	var userId = undefined;
     angular.element(document).ready(function () {
         var loc = (document.location).toString();
         var qryPar = loc.split("?")[1];
         var eachPar = qryPar.split(";");
         //var product = new Object();
-	  var productId = eachPar[0].split("=")[1];
+	  productId = eachPar[0].split("=")[1];
 	  //var productCount = eachPar[1].split("=")[1];
-	  var userId = 1;
+	  userId = 1;
 	  var host = $window.location.host;
 	  console.log(productId+"-----"+userId)
         $http({
