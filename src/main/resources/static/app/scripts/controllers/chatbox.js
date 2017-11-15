2c18a3a8b670da13e7426a5ca22b3f2143fb4df8
Kamesh Bandla <kbandla@nisum.com>
2017-11-15 21:28:41 +0530

    Chat Box Changes

--- src/main/resources/static/app/scripts/controllers/chatbox.js
+++ src/main/resources/static/app/scripts/controllers/chatbox.js
@@ -3,11 +3,26 @@ $(document).ready(function() {
 	$('<div/>', {
 	    id: 'chat-window'
 	}).appendTo('body');
+	
+	
 	$('<div/>', {
 	    id: 'chat-modal'
 	}).appendTo('#chat-window');
-	$('#chat-window').append('<button id="btnChatWindow">Chat</button>');
+	
+	$('#chat-window').append('<button id="btnChatWindow">Chat with us</button>');
+	
+	
 	$("#btnChatWindow").on("click", function() {
-		$("#chat-modal").load("app/views/chatWindow.html");
+		$("#chat-modal").load("chatWindow.html");
+		setTimeout(function(){
+			$(".ui-dialog").css("position","fixed");
+		})
+		
 	});
+	
+	setTimeout(function(){
+		$("#btnChatWindow").click();
+	},15000)
+	
+
 });
