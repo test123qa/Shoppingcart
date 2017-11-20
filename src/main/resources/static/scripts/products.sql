475a212375b0d96a1d2fadf730e2172f8dd8bd20
Kamesh Bandla <kbandla@nisum.com>
2017-11-20 18:06:24 +0530

    Product insertion query changes

--- src/main/resources/static/scripts/products.sql
+++ src/main/resources/static/scripts/products.sql
@@ -1,74 +1,74 @@
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-( "MEN", "Champion Men's Logo Graphic T-Shirt", "../images/1.jpeg", "T-Shirt", 1000, "MEN_ACTIVE_WARE", 35.75)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+( "MEN", "Champion Mens Logo Graphic T-Shirt", "../images/1.jpeg", "T-Shirt", 1000, "MEN_ACTIVE_WARE", 35.75);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-( "WOMEN", "adidas Men's Tricot Track Pants", "../images/2.jpeg", "Tracks", 500, "WOMEN_ACTIVE_WARE", 45.50)
-
-
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-( "WOMEN", "adidas Men's Essential Tricot Track Jacket", "../images/3.jpeg", "Shirt", 100, "WOMEN_ACTIVE_WARE", 1500.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+( "MEN", "adidas Mens Tricot Track Pants", "../images/2.jpeg", "Tracks", 500, "WOMEN_ACTIVE_WARE", 45.50);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("KIDS", "KIDS_ACTIVE_WARE", "../images/4.jpeg", "Leather bag imported from USA ", 400, "Office Bag", 1000.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+( "WOMEN", "adidas Mens Essential Tricot Track Jacket", "../images/3.jpeg", "Shirt", 100, "WOMEN_ACTIVE_WARE", 1500.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "adidas Men's Essential Tricot Track Jacket", "../images/10.jpeg", "Jackets", 800,  "MEN_ACTIVE_WARE", 450.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("KIDS", "KIDS_ACTIVE_WARE", "../images/4.jpeg", "Leather bag imported from USA ", 400, "Office Bag", 1000.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "adidas Originals Men's Fleece Trefoil Hoodie", "../images/5.jpeg", "Hoodies", 800, "MEN_ACTIVE_WARE", 2500.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "adidas Mens Essential Tricot Track Jacket", "../images/10.jpeg", "Jackets", 800,  "MEN_ACTIVE_WARE", 450.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Tech™ Short Sleeve Shirt", "../images/6.jpeg", "T-Shirt", 800,  "MEN_ACTIVE_WARE", 45000.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "adidas Originals Mens Fleece Trefoil Hoodie", "../images/5.jpeg", "Hoodies", 800, "MEN_ACTIVE_WARE", 2500.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Puma Men's P48 Core Track Jacket", "../images/7.jpeg", "Hoodies", 800,  "MEN_ACTIVE_WARE", 300.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Tech™ Short Sleeve Shirt", "../images/6.jpeg", "T-Shirt", 800,  "MEN_ACTIVE_WARE", 45000.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Champion Men's Vapor® Select Training Pants", "../images/8.jpeg", "Pants", 800, "MEN_ACTIVE_WARE", 550.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Puma Mens P48 Core Track Jacket", "../images/7.jpeg", "Hoodies", 800,  "MEN_ACTIVE_WARE", 300.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's 10 Tech Graphic Shorts", "../images/9.jpeg", "Shorts", 800,  "MEN_ACTIVE_WARE", 85000.0)
-
-//////////////////////////New Product////////////////////////
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Champion Mens Vapor® Select Training Pants", "../images/8.jpeg", "Pants", 800, "MEN_ACTIVE_WARE", 550.0);
 
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Tech Quarter-Zip Pullover", "../images/hoodies1.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 2500.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Men's 10 Tech Graphic Shorts", "../images/9.jpeg", "Shorts", 800,  "MEN_ACTIVE_WARE", 85000.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Storm Specialist Sweater", "../images/Hoodie2.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 2000.0)
+#//////////////////////////New Product////////////////////////
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Storm Swacket", "../images/hoodies3.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 1555.0)
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Archive Zip Hoodie", "../images/hoodies4.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 2100.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Tech Quarter-Zip Pullover", "../images/hoodies1.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 2500.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Tech Textured-Stripe Polo", "../images/T-shirt1.jpeg", "Polo T-shirt", 800, "MEN_ACTIVE_WARE", 1600.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Storm Specialist Sweater", "../images/Hoodie2.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 2000.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Men's Ferrari Polo", "../images/T-shirt2.jpeg", "Polo T-shirt", 800, "MEN_ACTIVE_WARE", 1200.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Storm Swacket", "../images/Hoodie3.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 1555.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's CoolSwitch Striped Polo", "../images/T-shirt3.jpeg", "Polo T-shirt", 800, "MEN_ACTIVE_WARE", 1500.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Archive Zip Hoodie", "../images/Hoodie4.jpeg", "Hoodie", 800, "MEN_ACTIVE_WARE", 2100.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Match Play Golf Shorts", "../images/shorts1.jpeg", "Shorts", 800, "MEN_ACTIVE_WARE", 900.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Tech Textured-Stripe Polo", "../images/T-shirt1.jpeg", "Polo T-shirt", 800, "MEN_ACTIVE_WARE", 1600.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Under Armour Men's Phantom Blackball 18 Boardshorts", "../images/shorts2.jpeg", "Shorts", 800, "MEN_ACTIVE_WARE", 1000.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Mens Ferrari Polo", "../images/T-shirt2.jpeg", "Polo T-shirt", 800, "MEN_ACTIVE_WARE", 1200.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Gold Toe Cotton Quarter Athletic 6 Pack Socks", "../images/socks1.jpeg", "Socks", 800, "MEN_ACTIVE_WARE", 300.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens CoolSwitch Striped Polo", "../images/T-shirt3.jpeg", "Polo T-shirt", 800, "MEN_ACTIVE_WARE", 1500.0);
 
-insert into nisumshoppingcart.product (category, description, image_url, name, quantity, sub_category, unit_price) values 
-("MEN", "Gold Toe Socks, Athletic Cushion Liner 4 Pack", "../images/socks2.jpeg", "Socks", 800, "MEN_ACTIVE_WARE", 200.0)
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Men's Match Play Golf Shorts", "../images/shorts1.jpeg", "Shorts", 800, "MEN_ACTIVE_WARE", 900.0);
+
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Under Armour Mens Phantom Blackball 18 Boardshorts", "../images/shorts2.jpeg", "Shorts", 800, "MEN_ACTIVE_WARE", 1000.0);
+
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Gold Toe Cotton Quarter Athletic 6 Pack Socks", "../images/socks1.jpeg", "Socks", 800, "MEN_ACTIVE_WARE", 300.0);
+
+insert into product (category, description, image_url, name, quantity, subCategory, unit_price) values 
+("MEN", "Gold Toe Socks, Athletic Cushion Liner 4 Pack", "../images/socks2.jpeg", "Socks", 800, "MEN_ACTIVE_WARE", 200.0);
 
